25c436839d789d31a9d418dc52589713e037ff39
arif <arifahmadalfian@gmail.com>
2022-07-16 10:46:05 +0700

    splashscreen & dependencies lifecycleScope

--- app/src/main/java/com/company/qrcode2/SplashScreen.kt
+++ app/src/main/java/com/company/qrcode2/SplashScreen.kt
@@ -1,9 +1,14 @@
 package com.company.qrcode2
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.company.qrcode2.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
@@ -15,6 +20,18 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ivIconStmik.alpha = 0f
+
+        binding.ivIconStmik.animate()?.setDuration(1500)?.alpha(1f)?.withEndAction {
+            lifecycleScope.launch(Dispatchers.Main) {
+                delay(1000) // wait for 1 second
+                val intent = Intent(this@SplashScreen, Login::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
+        }
     }
 
     override fun onDestroy() {
