4b101785ca541111c6862033cf31169bc6478623
arif <arifahmadalfian@gmail.com>
2022-07-16 13:35:07 +0700

    register

--- app/src/main/java/com/company/qrcode2/Register.kt
+++ app/src/main/java/com/company/qrcode2/Register.kt
@@ -1,8 +1,8 @@
 package com.company.qrcode2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.company.qrcode2.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
@@ -15,8 +15,47 @@ class Register : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnRegister.setOnClickListener {
-            //succes
-            finish() // clear activity
+            val nim = binding.etNim.text.toString()
+            val nama = binding.etNama.text.toString()
+            val prodi = binding.etProdi.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPasswrod.text.toString()
+
+            /**
+             * Disini belum menggunakan pengecekan apakah semua field terisi atau tidak
+             * silahkan tambahkan pengecekan apakah semua field terisi atau tidak (optional)
+             */
+
+            val client = ApiConfig.getApiService().postRegister(
+                nim,
+                nama,
+                prodi,
+                email,
+                password
+            )
+            client.enqueue(object : retrofit2.Callback<Response> {
+                override fun onFailure(call: retrofit2.Call<Response>, t: Throwable) {
+                    Toast.makeText(
+                        this@Register,
+                        "Register Gagal",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onResponse(
+                    call: retrofit2.Call<Response>,
+                    response: retrofit2.Response<Response>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(
+                            this@Register,
+                            "Register Success, Anda akan di arahkan ke halaman Login",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+            })
         }
     }
 
